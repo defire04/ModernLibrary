877a28437cf003202d1fba33e6682c0a96494471
DeFire04 <defire04st@gmail.com>
2024-01-15 21:03:44 +0200

    Initial commit

---
+++ src/main/java/com/example/library/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDTO<T> {
+    private T data;
+
+}
---
+++ src/main/java/com/example/library/model/Book.java
@@ -0,0 +1,34 @@
+package com.example.library.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book extends BaseEntity {
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "publication_year")
+    private int publicationYear;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "available")
+    private boolean available;
+
+
+
+}
