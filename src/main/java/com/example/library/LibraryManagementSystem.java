a63c54f520634928d310c867dbf2e7c3b3bab2ef
Dima <defire04st@gmail.com>
2024-06-13 15:54:10 +0300

    Create builder pattern

--- src/main/java/com/example/library/LibraryManagementSystem.java
+++ src/main/java/com/example/library/LibraryManagementSystem.java
@@ -1,19 +1,18 @@
 package com.example.library;
 
-import com.example.library.factory.BookFactory;
-import com.example.library.factory.FictionBookFactory;
-import com.example.library.factory.NonFictionBookFactory;
 import com.example.library.model.Book;
 
 public class LibraryManagementSystem {
     public static void main(String[] args) {
-        BookFactory fictionFactory = new FictionBookFactory();
-        Book fictionBook = fictionFactory.createBook("1984", "George Orwell");
+        Book book = new Book.BookBuilder()
+                .setTitle("The Great Gatsby")
+                .setAuthor("F. Scott Fitzgerald")
+                .setPublicationYear(1925)
+                .setGenre("Fiction")
+                .setPrice(15.99)
+                .setAvailable(true)
+                .build();
 
-        BookFactory nonFictionFactory = new NonFictionBookFactory();
-        Book nonFictionBook = nonFictionFactory.createBook("Sapiens", "Yuval Noah Harari");
-
-        System.out.println("Fiction Book: " + fictionBook.getTitle() + " by " + fictionBook.getAuthor() + " [" + fictionBook.getGenre() + "]");
-        System.out.println("Non-Fiction Book: " + nonFictionBook.getTitle() + " by " + nonFictionBook.getAuthor() + " [" + nonFictionBook.getGenre() + "]");
+        System.out.println("Book: " + book.getTitle() + " by " + book.getAuthor());
     }
 }
--- src/main/java/com/example/library/model/Book.java
+++ src/main/java/com/example/library/model/Book.java
@@ -1,16 +1,67 @@
 package com.example.library.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-public abstract class Book {
+@Setter
+@Getter
+public class Book {
     private String title;
     private String author;
+    private int publicationYear;
+    private String genre;
+    private double price;
+    private boolean available;
 
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
+    private Book(BookBuilder builder) {
+        this.title = builder.title;
+        this.author = builder.author;
+        this.publicationYear = builder.publicationYear;
+        this.genre = builder.genre;
+        this.price = builder.price;
+        this.available = builder.available;
     }
 
-    public abstract String getGenre();
+    public static class BookBuilder {
+        private String title;
+        private String author;
+        private int publicationYear;
+        private String genre;
+        private double price;
+        private boolean available;
+
+        public BookBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BookBuilder setAuthor(String author) {
+            this.author = author;
+            return this;
+        }
+
+        public BookBuilder setPublicationYear(int year) {
+            this.publicationYear = year;
+            return this;
+        }
+
+        public BookBuilder setGenre(String genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public BookBuilder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public BookBuilder setAvailable(boolean available) {
+            this.available = available;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
 }
